90e36af034be7080adb6860f72886d668a0ebaaf
hzqsns <787624681@qq.com>
2025-05-18 14:19:14 +0800

    feat: 添加首页标签

--- apps/sakura-frontend/src/components/layout/Header.tsx
+++ apps/sakura-frontend/src/components/layout/Header.tsx
@@ -53,6 +53,17 @@ export function Header() {
                 <NavigationMenu>
                     <NavigationMenuList>
                         <NavigationMenuItem>
+                            <Link to="/">
+                                <div
+                                    className={`group inline-flex h-10 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 ${
+                                        isActive('/') ? 'bg-gray-100/50  font-bold' : ''
+                                    }`}
+                                >
+                                    首页
+                                </div>
+                            </Link>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
                             <Link to="/about">
                                 <div
                                     className={`group inline-flex h-10 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 ${
